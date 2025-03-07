862c3fd8be2e5a6cea698663759ce89cbdfa4776
nhulnt <nhule27082003@gmail.com>
2025-03-07 07:47:43 +0700

    Fix code
    Fix code

--- src/bookstoremanagementsystem/models/Authors.java
+++ src/bookstoremanagementsystem/models/Authors.java
@@ -11,27 +11,11 @@ package bookstoremanagementsystem.models;
  */
 public class Authors {
 
-    private String authorID;
-    private String fullName;
+    String authorID;
+    String fullName;
 
     public Authors(String authorID, String fullName) {
         this.authorID = authorID;
-        this.fullName = fullName;
-    }
-
-    public String getAuthorID() {
-        return authorID;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setAuthorID(String authorID) {
-        this.authorID = authorID;
-    }
-
-    public void setFullName(String fullName) {
         this.fullName = fullName;
     }
 }
